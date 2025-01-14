6a2b347129bc386391d15f1f82a33da431f09069
KillianRipoche <killian.ripoche@gmail.com>
2025-01-14 09:38:51 +0100

    Fix 80 colonnes

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+Last_g = abs(number) % 10
 if number < 0:
-    last_digit = -last_digit
+    Last_g = -Last_g
 
-if last_digit > 5:
-    print(f"last_digit of {number} is {last_digit} and is greater than 5")
+if Last_g > 5:
+    print(f"Last_digit of {number} is {Last_g} and is greater than 5")
 
-elif last_digit == 0:
-    print(f"last_digit of {number} is {last_digit} and is 0")
+elif Last_g == 0:
+    print(f"Last_digit of {number} is {Last_g} and is 0")
 
 else:
-    print(f"last_digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last_digit of {number} is {Last_g} and is less than 6 and not 0")
