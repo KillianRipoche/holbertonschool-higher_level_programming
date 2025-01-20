09847b4a375e54131ef63a21c09d8a8e1c3d34a0
KillianRipoche <killian.ripoche@gmail.com>
2025-01-20 14:58:49 +0100

    Change result

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = []
+    result = ""
 
     for char in my_string:
         if char != 'c' and char != 'C':
-            result.append[char]
+            result += char
 
-    return "".join(result)
+    return result
