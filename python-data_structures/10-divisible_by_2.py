6ef7d32c35de00d6c990fd6a036eb5c7b27af7d7
KillianRipoche <killian.ripoche@gmail.com>
2025-01-21 10:10:53 +0100

    by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+
+    result = []
+
+    for i in my_list:
+        if i % 2 == 00:
+            result.append(True)
+        else:
+            result.append(False)
+
+    return result
