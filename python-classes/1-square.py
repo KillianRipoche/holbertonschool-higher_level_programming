627e228e1570924b7bc8ce7dc713a4f963153bba
KillianRipoche <killian.ripoche@gmail.com>
2025-01-27 13:42:34 +0100

    Size of the square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Create a new class, we'll define it with size
+"""
+
+
+class Square:
+    """this is a square with size
+    """
+    def __init__(self, size):
+        self.__size = size
