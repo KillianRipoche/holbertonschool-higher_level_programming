42ad6a8808e53ccb107f8c7f31962a06c6863497
KillianRipoche <killian.ripoche@gmail.com>
2025-01-27 13:56:34 +0100

    Square with error

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Create a new class, we'll define it with size
+"""
+
+
+class Square:
+    """this is a square with size and error
+    """
+
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
