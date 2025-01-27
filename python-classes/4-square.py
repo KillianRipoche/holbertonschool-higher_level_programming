93a88519a356efe06394e944efd7c5a5c81f45ca
KillianRipoche <killian.ripoche@gmail.com>
2025-01-27 15:46:21 +0100

    Property and size setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Create a new class, we'll define it with size
+"""
+
+
+class Square:
+    """this is a square with size and error
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter for size value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
