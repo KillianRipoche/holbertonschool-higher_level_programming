fbf36dd7fda16a23b67f48a55a5e8bb3fd25c0c8
KillianRipoche <killian.ripoche@gmail.com>
2025-02-05 10:26:09 +0100

    add com

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-This module provides a function to list all available attributes and methods of an object.
-Returns:
-list: A list of attribute and method names available in the object.
+    This module provides a function to list all available attributes and methods of an object.
+    Returns:
+    list: A list of attribute and method names available in the object.
 """
 
 
