bdeba5b877c217ce02149955df813ff3133b827f
KillianRipoche <killian.ripoche@gmail.com>
2025-02-05 09:41:00 +0100

    add dir

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module provides a function to list all available attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    """
+
+    return dir(obj)
