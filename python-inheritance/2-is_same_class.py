d67facc6eaeb8c824386a2dd5306f7495c1eb09d
KillianRipoche <killian.ripoche@gmail.com>
2025-02-05 13:02:09 +0100

    fix com

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""
-This module provides a function to check if an object is exactly an instance of a specified class.
+"""Module that contains a function to check
+if an object is exactly an instance of a specified class.
 """
 
 
 def is_same_class(obj, a_class):
     """
-    Checks if an object is exactly an instance of the specified class.
+    Checks if the object is exactly an instance of the specified class.
+
+    :param obj: The object to check
+    :param a_class: The class to compare with
+    :return: True if obj is exactly an instance of a_class, otherwise False
     """
     return type(obj) is a_class
--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a class BaseGeometry with a method that raises an exception.
+This module defines a class BaseGeometry with a method that raises
+an exception.
 """
 
 
