1a58d4797718079dd6e81dd2d6d58040519d0067
KillianRipoche <killian.ripoche@gmail.com>
2025-02-11 10:11:34 +0100

    Thanks pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     for i in range(n):
 
         add = [None] * (i + 1)
-        add[0] =   add[-1] = 1
+        add[0] = add[-1] = 1
 
         for j in range(1, i):
             add[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
