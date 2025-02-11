1ea25b66e583c4646f570cbe3ce68d076416183d
KillianRipoche <killian.ripoche@gmail.com>
2025-02-11 17:15:50 +0100

    Task 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python3
-"""this is a com"""
-import json
-"""thanks checker"""
+#!/usr/bin/python3
+"""initialize and empty dictionary to be serialized."""
 
+
+import json
 
 def serialize_and_save_to_file(data, filename):
-    """
-    save file
-    """
-    with open(filename, 'w') as file:
+    """serialize and save data to the specified file."""
+    with open(filename, "w") as file:
         json.dump(data, file)
 
 
 def load_and_deserialize(filename):
-    """
-    Load and deserialize
-    """
-    with open(filename, 'r') as file:
+    """load and deserialize data from the specified file."""
+    with open(filename, "r") as file:
         data = json.load(file)
     return data
