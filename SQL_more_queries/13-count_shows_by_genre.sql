08fe5efbcd6e71bad5a8fffbb2cbc6b45cdd6ed6
KillianRipoche <killian.ripoche@gmail.com>
2025-03-05 14:20:34 +0100

    Another one

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,10 +3,9 @@
 -- Only genres with at least one TV show are displayed.
 -- The output displays two columns: genre and number_of_shows,
 -- sorted in descending order by the number of shows.
-SELECT
-    genres.name AS genre,
-    COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.id
+SELECT g.name AS genre, COUNT(g.id) AS number_of_shows
+FROM tv_genres g
+JOIN tv_show_genres tsg ON g.id = tsg.genre_id
+GROUP BY g.id
 ORDER BY number_of_shows DESC;
+
