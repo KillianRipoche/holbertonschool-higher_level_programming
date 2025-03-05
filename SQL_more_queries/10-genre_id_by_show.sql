cf9fa1c31cd2df7cadd2032343098b1d8984c915
KillianRipoche <killian.ripoche@gmail.com>
2025-03-05 13:52:24 +0100

    task 11 to 16

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,4 @@
--- List all shows with at least one linked genre
--- displaying tv_shows.title and tv_show_genres.genre_id
--- Results are sorted in ascending order
+-- List all shows with at least one linked genre displaying tv_shows.title and tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
