9d70a4c12afb6311d92f83a165767c2e3a0c5ed8
KillianRipoche <killian.ripoche@gmail.com>
2025-03-05 12:44:57 +0100

    add com

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,6 @@
--- List all shows with at least one linked genre, displaying tv_shows.title and tv_show_genres.genre_id
+-- List all shows with at least one linked genre
+-- displaying tv_shows.title and tv_show_genres.genre_id
+-- Results are sorted in ascending order
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,8 @@
 -- List all cities of California sorted by cities.id in ascending order without using JOIN
 SELECT *
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California')
 ORDER BY id ASC;
