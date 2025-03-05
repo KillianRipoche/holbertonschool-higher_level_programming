590b249a8edd3287a2eaee4053a78d7cdda77ab6
KillianRipoche <killian.ripoche@gmail.com>
2025-03-05 11:53:31 +0100

    Fix my task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- Create the user user_0d_1 with password user_0d_1_pwd if it doesn't already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- Reload the privileges to ensure the changes take effect immediately
-FLUSH PRIVILEGES;
