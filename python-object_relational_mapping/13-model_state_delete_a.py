41d91c3965d93ddca822307e349abf43c1019648
KillianRipoche <killian.ripoche@gmail.com>
2025-03-06 14:38:38 +0100

    Not the good copy

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    state = session.query(State).filter(State.id.like(2)).first()
-    state.name = 'New Mexico'
+    for state in session.query(State).filter(State.name.contains('a')):
+        session.delete(state)
     session.commit()
     session.close()
